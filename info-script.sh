9f40952a77552ddb10478b96547ce9f55fd4c407
William <william.friden@gmail.com>
2025-12-09 14:56:31 +0100

    Min första commit, lade till readme fil :D

---
+++ info-script.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+# Detta script samlar in systeminformation - RECON
+#
+# Kan användas för följande attacker:
+# [Skriv möjliga attacker]
+#
+# Author: Frans Schartau
+# Last Update: 2025-01-01
+
+
+echo "Välkommen till RECON script för att kontrollera en Linux-miljö"
+
+echo
+echo "=== SYSTEMINFO ==="
+echo "=== OS, KERNEL, ARKITEKTUR ==="
+uname -a
+env
+echo
+
+echo
+echo "=== AKTUELL ANVÄNDARE ==="
+echo $USER
+echo
+
+echo
+echo "=== ANVÄNDARE MED SHELL ==="
+grep "sh$" /etc/passwd
+lslogin #Rolig User info :)
+echo
+
+echo
+echo "=== NÄTVERK ==="
+ip a | grep inet
+curl https://ipapi.co/json/
+echo
+
+echo
+echo "=== HÅRDVARA  ==="
+lscpu
+free -h
+echo
+
+info-script.sh > utdata
